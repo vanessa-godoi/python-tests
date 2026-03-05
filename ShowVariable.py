22de3c63f53e9c8a5c44a3525b1fe2fcd7c8f06e
vanessa-godoi <nessagodoiii@gmail.com>
2026-03-05 15:53:20 -0300

    test-variables

---
+++ ShowVariable.py
@@ -0,0 +1,9 @@
+code = 105
+name = 'Justin Crawford'
+salary = 1650.00
+active = True
+
+print("Code: %d "% code)
+print("Name: %s "% name)
+print("Salary: %s "% salary)
+print("Active: %r "% active)
