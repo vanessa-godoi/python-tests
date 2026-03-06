89e8815d89445a5fcf607abbf07a6fa0153bfd3a
vanessa-godoi <nessagodoiii@gmail.com>
2026-03-06 16:10:47 -0300

    test-show-variable

---
+++ show-variable.py
@@ -0,0 +1,9 @@
+code = int(input("Write the colaborator code: "))
+name = input("Write the name of the colaborator: ")
+salary = float(input("Write the salary of the colaborator: "))
+active = True
+
+print("Code: %d "% code)
+print("Name: %s "% name)
+print("Salary: %.2f "% salary)
+print("Active: %r " % active)
