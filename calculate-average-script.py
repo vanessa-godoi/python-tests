09690a441fc60302a8d84cbfbfe7599cce361079
vanessa-godoi <nessagodoiii@gmail.com>
2026-03-06 16:23:41 -0300

    add-calculate-average-2

--- calculate-average-script.py
+++ calculate-average-script.py
@@ -1,3 +1,5 @@
+# SCRIPT 1
+
 noteA = float (input ("Write the first note: "))
 noteB = float (input ("Write the second note: "))
 
@@ -9,3 +11,23 @@ if finalaverage >= 7.0:
     print("Average: %.1f - Approved "% finalaverage)
 else:
     print("Average: %.1f - Not Approved "% finalaverage)
+
+
+# SCRIPT 2
+
+quantity = 0
+sum = 0
+average = 0
+value = float (input ("Write a value: "))
+
+while (value > 0.0):
+    sum = sum + value
+    quantity = quantity + 1
+    #values input
+    value = float (input ("Write a value: "))
+
+#if a negative value is entered, the loop ends.
+average = sum / quantity
+print("\n Total sum: ", sum)
+print("\n Quantity of values entered: ", quantity)
+print("\n Average value: ", average)
