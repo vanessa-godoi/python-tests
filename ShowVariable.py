3b430b61d02e72e1378f652368d7d0111b52df88
vanessa-godoi <nessagodoiii@gmail.com>
2026-03-06 15:59:47 -0300

    delete-showvariable-file

--- ShowVariable.py
+++
@@ -1,9 +0,0 @@
-code = 105
-name = 'Justin Crawford'
-salary = 1650.00
-active = True
-
-print("Code: %d "% code)
-print("Name: %s "% name)
-print("Salary: %s "% salary)
-print("Active: %r "% active)
