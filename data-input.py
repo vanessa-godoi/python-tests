0c913e877798c1d70b00acc6d8f0df0579eb5c7a
vanessa-godoi <nessagodoiii@gmail.com>
2026-03-06 15:51:49 -0300

    test-data-input

---
+++ data-input.py
@@ -0,0 +1,9 @@
+code = int(input("Enter the contributor code: "))
+name = input("Enter the contributor name: ")
+salary = float(input("Enter the contributor salary: "))
+active = True
+
+print("Code: %d "% code)
+print("Name: %s "% name)
+print("Salary: %s "% salary)
+print("Active: %r "% active)
